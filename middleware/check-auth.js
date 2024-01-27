552863e3493facfb3bed801250f47ffe43ad0add
Zichen Men <zmen@Zichens-MacBook-Air.local>
2024-01-26 20:31:22 -0800

    changed starting script

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
     if (!token) {
-      throw new Error("Authentication failed!");
+      throw new Error("Authentication failed");
     }
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = { userId: decodedToken.userId };
